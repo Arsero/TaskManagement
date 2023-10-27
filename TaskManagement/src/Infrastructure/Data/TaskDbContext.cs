fbbbec49e85cd1b1997ba6c0ef031fb8e0a6b090
Arsero <54241944+Arsero@users.noreply.github.com>
2023-10-27 23:36:05 +0200

    refactor: move seeding db data

--- TaskManagement/src/Infrastructure/Data/TaskDbContext.cs
+++ TaskManagement/src/Infrastructure/Data/TaskDbContext.cs
@@ -9,6 +9,7 @@ namespace Infrastructure.Data
         public DbSet<Comment> Comments { get; set; }
         public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
         {
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
@@ -20,10 +21,7 @@ namespace Infrastructure.Data
                 .HasForeignKey(c => c.TaskId)
                 .IsRequired();
 
-            modelBuilder.Entity<Domain.Entities.Task>()
-                .HasData(new Domain.Entities.Task { Id = 1, Title = "First task", Description = "Okay let's go !", DueDate = DateTime.Today.AddDays(1), IsCompleted = false },
-            new Domain.Entities.Task { Id = 2, Title = "Second task", Description = "Okay let's go ! 2", DueDate = DateTime.Today.AddDays(2), IsCompleted = false },
-            new Domain.Entities.Task { Id = 3, Title = "Third task", Description = "Okay let's go ! 3", DueDate = DateTime.Today.AddDays(3), IsCompleted = true });
+            new TaskDbContextInitialiser(modelBuilder).Seed();
         }
     }
 }
